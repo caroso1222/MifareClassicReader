5d9bc733b390437d17523c6b3c85f42cde4c8148
carlos <ce.roso398@gmail.com>
2014-03-27 14:00:43 -0500

    lector mifare classic funcional

--- src/com/example/mifareclassicreader/MainActivity.java
+++ src/com/example/mifareclassicreader/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mifareclassicreader;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -14,6 +18,7 @@ import android.nfc.Tag;
 import android.nfc.tech.MifareClassic;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,14 +27,32 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 	
 	private Intent mOldIntent = null;
 	private NfcAdapter mAdapter;
 	private TextView tv;
-
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	
+	/**
+	 * Filtro para activar/desactivar el foreground dispatch.
+	 */
+	private IntentFilter[] intentFiltersArray;
+	
+	/**
+	 * Lista de tecnologías que soporta la app.
+	 */
+	private String[][] techListsArray;
+	
+	/**
+	 * PendingIntent que utiliza el Foreground Dispatch System (FDS)
+	 */
+	private PendingIntent pendingIntent;
+	
+	
 	/**
      * Check for NFC hardware, Mifare Classic support and for external storage.
      * If the directory structure and the std. keys files is not already there
@@ -41,21 +64,28 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "Empieza aplicación", Toast.LENGTH_SHORT).show();
+        
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         mAdapter = NfcAdapter.getDefaultAdapter(this);
         tv = (TextView) findViewById(R.id.textView1);
-        tv.setText("buenas buenas");
-        if(mAdapter != null){
-        	tv.setText("1");
-        }       
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        tv.setText("");
+        tv.setTextSize(12);
+        if(mAdapter == null){
+        	tv.setText("Encienda el NFC de su dispositivo");
+        }         
         
+        /** Se hace un PendingIntent para darle prioridad a esta aplicación
+         *  de que se quede abierta cuando se detecte un tag. Android "llena" este intent
+         *  con los detalles del tag cuando se escanea.
+         */
+        
+        
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter[] intentFiltersArray = new IntentFilter[] {ndef, };    
+        String[][] techListsArray = new String[][] { new String[] { MifareClassic.class.getName() } };
         
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -94,33 +124,85 @@ public class MainActivity extends ActionBarActivity {
     }
     
     /**
-     * Handle new Intent as a new tag Intent and if the tag/device does not
-     * support Mifare Classic, then run {@link TagInfoTool}.
-     * @see Common#treatAsNewTag(Intent, android.content.Context)
-     * @see TagInfoTool
+     * Cuando la actividad pierde focus se deshabilita el FDS.
      */
-    @Override
-	protected void onNewIntent(Intent intent){
-    	tv.setText("holaquetal");
-    	/**
-		if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
-		Tag mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);  // get the detected tag
-		Parcelable[] msgs =
-intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-			NdefRecord firstRecord = ((NdefMessage)msgs[0]).getRecords()[0];
-			byte[] payload = firstRecord.getPayload();
-			int payloadLength = payload.length;
-			int langLength = payload[0];
-			int textLength = payloadLength - langLength - 1;
-			byte[] text = new byte[textLength];
-			System.arraycopy(payload, 1+langLength, text, 0, textLength);
-			Toast.makeText(this, this.getString(R.string.ok_detection)+new String(text), Toast.LENGTH_LONG).show();
-					}
-					*/
-	}
+    public void onPause(){
+    	super.onPause();
+    	mAdapter.disableForegroundDispatch(this);
+    }
+    
+    /**
+     * Cuando se reanuda la actividad se habilita el FDS.
+     */
+    public void onResume(){
+    	super.onResume();
+        mAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
+    }
+    
+    public void onNewIntent(Intent intent) {
+    	tv.setText("");
+		Toast.makeText(getApplicationContext(), "Tag detectado!!!", Toast.LENGTH_SHORT).show();   	
+		tv.append("Technology: \n");
+    	Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);   
+        for (String tech : tagFromIntent.getTechList()) {   
+            tv.append(tech + "\n");
+        }   
 
+        boolean autenticado = false;   
+        MifareClassic mfc = MifareClassic.get(tagFromIntent);   
+        try {   
+            String metaInfo = "";   
+            //Enable I/O operations to the tag from this TagTechnology object.   
+            mfc.connect();   
+            int type = mfc.getType(); 
+            int sectorCount = mfc.getSectorCount();   
+            String typeS = "";   
+            switch (type) {   
+            case MifareClassic.TYPE_CLASSIC:   
+                typeS = "TYPE_CLASSIC";   
+                break;   
+            case MifareClassic.TYPE_PLUS:   
+                typeS = "TYPE_PLUS";   
+                break;   
+            case MifareClassic.TYPE_PRO:   
+                typeS = "TYPE_PRO";   
+                break;   
+            case MifareClassic.TYPE_UNKNOWN:   
+                typeS = "TYPE_UNKNOWN";   
+                break;   
+            }   
+            metaInfo += "Card type：" + typeS + " with " + sectorCount + " Sectors, "   
+                    + mfc.getBlockCount() + " Blocks Storage Space: " + mfc.getSize() + "B\n";   
+            for (int j = 0; j < sectorCount; j++) {   
+                //Authenticate a sector with key A.   
+                autenticado = mfc.authenticateSectorWithKeyA(j, MifareClassic.KEY_DEFAULT);   
+                int bCount;   
+                int bIndex;   
+                if (autenticado) {   
+                    metaInfo += "Sector " + j + ": Verified successfully\n";   
+                    bCount = mfc.getBlockCountInSector(j);   
+                    bIndex = mfc.sectorToBlock(j);   
+                    for (int i = 0; i < bCount; i++) {   
+                        byte[] data = mfc.readBlock(bIndex); 
+                        /*
+                        String str = new String(data, Charset.forName("US-ASCII"));
+                        metaInfo += "Block " + bIndex + " : " + str + "\n";*/   
+                        metaInfo += "Block " + bIndex + " : " + bytesToHex(data) + "\n";
+                        bIndex++;   
+                    }   
+                } else {   
+                    metaInfo += "Sector " + j + ": Verified failure\n";   
+                }   
+            }   
+            tv.append(metaInfo);   
+        } catch (Exception e) {   
+            e.printStackTrace();   
+        }   
+    	
+    	
+    }
     
-    
+    /**
     private void processIntent(Intent intent) {    
         Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);   
         for (String tech : tagFromIntent.getTechList()) {   
@@ -176,5 +258,63 @@ intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
             e.printStackTrace();   
         }   
     }   
+    */
     
+	public String readTag(Tag tag) {
+		//Hace una instancia de un tag Mifare Classic
+        MifareClassic mifare = MifareClassic.get(tag);
+        try {
+        	//Abre la comunicación con el tag
+            mifare.connect();
+            //Autentica el sector 0
+            mifare.authenticateSectorWithKeyA(0,hexStringToByteArray("FFFFFFFFFFFF"));
+            //Lee el bloque 0
+            byte[] payload = mifare.readBlock(0);
+            //Retorna lo que lee en el bloque 0 en formato String
+            return new String(payload, Charset.forName("US-ASCII"));
+        } catch (IOException e) {
+        	//Se encarga de los errores que haya en la lectura
+        	Log.e(mifare.getClass().getSimpleName(), "IOException while closing MifareClassic...", e);
+        } finally {
+            if (mifare != null) {
+               try {
+            	   //Cerrar la comunicación con el tag
+                   mifare.close();
+               }
+               catch (IOException e) {
+                   Log.e(mifare.getClass().getSimpleName(), "Error closing tag...", e);
+               }
+            }
+        }
+        return null;
+    }
+	
+	private static byte[] hexStringToByteArray(String s) {
+	    int len = s.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+	                             + Character.digit(s.charAt(i+1), 16));
+	    }
+	    return data;
+	}
+	
+	private void processIntent(Intent intent){
+Toast.makeText(getApplicationContext(), "Tag detectado!!!", Toast.LENGTH_SHORT).show();   	
+    	
+    	Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);   
+        for (String tech : tagFromIntent.getTechList()) {   
+            tv.append(tech + "\n");
+        }
+	}
+	
+	private String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
 }
